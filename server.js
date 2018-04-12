16a803994230bd8eb5699cc3e0c3d65eefad8a21
mateuszdziuba <mkdziuba@gmail.com>
2018-04-13 01:27:41 +0200

    database

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const profile = require('./controllers/profile');
 const db = knex({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
-      user : 'postgres',
-      password : 'lolok123',
-      database : 'smart-brain'
+        host : process.env.DATABASE_URL,
+        ssl: true
     }
 });
 
