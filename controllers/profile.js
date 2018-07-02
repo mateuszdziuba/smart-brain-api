ab6c7527b48f410e6577e28874c076ec37c0a83e
mateuszdziuba <mkdziuba@gmail.com>
2018-07-03 00:42:12 +0200

    dockerized the project, added autorization

--- controllers/profile.js
+++ controllers/profile.js
@@ -1,16 +1,37 @@
-const handleProfile = (req, res, db) => {
+const handleProfileGet = (req, res, db) => {
   const { id } = req.params;
   db.select('*').from('users').where({id})
-      .then(user => {
-          if (user.length) {
-              res.json(user[0])
-          } else {
-              res.status(400).json('not found')
-          }
+    .then(user => {
+      if (user.length) {
+        res.json(user[0])
+      } else {
+        res.status(400).json('Not found')
+      }
+    })
+    .catch(err => res.status(400).json('error getting user'))
+}
+
+const handleProfileUpdate = (req, res, db) => {
+  const { id } = req.params
+  const { name, age, pet } = req.body.formInput
+  db('users')
+  .where({ id })
+  .update({
+    name: name,
+    age: age,
+    pet: pet,
+   })
+  .then(resp => {
+    if (resp) {
+      res.json("success")
+    } else {
+      res.status(400).json('Not found')
+    }
   })
-  .catch(err => res.status(400).json('error getting user'))
+  .catch(err => res.status(400).json('error updating user'))
 }
 
 module.exports = {
-  handleProfile: handleProfile
+  handleProfileGet,
+  handleProfileUpdate
 }
--- server.js
+++ server.js
@@ -3,39 +3,34 @@ const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
+const morgan = require('morgan');
 
 const register = require('./controllers/register');
 const signin = require('./controllers/signin');
 const image = require('./controllers/image');
 const profile = require('./controllers/profile');
+const auth = require('./controllers/authorization');
 
 const db = knex({
     client: 'pg',
-    connection: {
-        connectionString : process.env.DATABASE_URL,
-        ssl: true
-    }
+    connection: process.env.POSTGRES_URI
 });
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(cors());
+app.use(morgan('combined'));
 
-app.get('/', (req,res) => {
-    res.send('it is working!');
-})
-
-app.post('/signin', signin.handleSignin(db, bcrypt))
-
+app.get('/', (req,res) => res.send('it is working!'))
+app.post('/signin', signin.signinAuthentication(db, bcrypt))
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
-
-app.get('/profile/:id', (req, res) => { profile.handleProfile(req, res, db) })
-
-app.put('/image', (req, res) => { image.handleImage(req, res, db) })
-
-app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) })
+app.get('/profile/:id', auth.requireAuth, (req, res) => { profile.handleProfileGet(req, res, db) })
+app.post('/profile/:id', auth.requireAuth, (req, res) => { profile.handleProfileUpdate(req, res, db) })
+app.put('/image', auth.requireAuth, (req, res) => { image.handleImage(req, res, db) })
+app.post('/imageurl', auth.requireAuth, (req, res) => { image.handleApiCall(req, res) })
 
 app.listen(process.env.PORT || 3000, () => {
-    console.log(`app is running on port ${process.env.PORT}`);
+    console.log(`app is running on port ${process.env.PORT || 3000}`);
 })
+
