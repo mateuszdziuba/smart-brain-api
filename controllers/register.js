a0e3995307907fdd740de1650d51198b9a7a57ae
mateuszdziuba <mkdziuba@gmail.com>
2018-07-03 16:00:11 +0200

    added authentication on register

--- controllers/register.js
+++ controllers/register.js
@@ -1,32 +1,100 @@
+const jwt = require('jsonwebtoken');
+
+const redisClient = require('./signin').redisClient;
+
 const handleRegister = (req, res, db, bcrypt) => {
-  const { email, name, password } = req.body;
+  const {
+    email,
+    name,
+    password
+  } = req.body;
   if (!email || !name || !password) {
-      return res.status(400).json('incorrect form submission');
+    return Promise.reject('incorrect form submission');
   }
   const hash = bcrypt.hashSync(password);
-      db.transaction(trx => {
-          trx.insert({ hash, email })
-          .into('login')
-          .returning('email')
-          .then(loginEmail => {
-              return trx('users')
-              .returning('*')
-              .insert({
-                  email: loginEmail[0],
-                  name: name,
-                  joined: new Date()   
-              })
-              .then(user => {
-                  res.json(user[0]);
-              })
-          })
-          .then(trx.commit)
-          .catch(trx.rollback)
-      })
+  return db.transaction(trx => {
+      trx.insert({
+          hash,
+          email
+        })
+        .into('login')
+        .returning('email')
+        .then(loginEmail => {
+          return trx('users')
+            .returning('*')
+            .insert({
+              email: loginEmail[0],
+              name: name,
+              joined: new Date()
+            })
+            .then(user => user[0])
+        })
+        .then(trx.commit)
+        .catch(trx.rollback)
+    })
 
-  .catch(err => res.status(400).json('unable to register'))
+    .catch(err => Promise.reject('unable to register'))
+}
+
+const signToken = (email) => {
+  const jwtPayload = { email };
+  return jwt.sign(jwtPayload, 'JWT_SECRET', { expiresIn: '2 days' });
+}
+
+const setToken = (token, id) => {
+  return Promise.resolve(redisClient.set(token, id))
+}
+
+const createSessions = (user) => {
+  const { id, email } = user;
+  const token = signToken(email);
+  return setToken(token, id)
+  .then(() => {
+    return { success: 'true', userId: id, token }
+  })
+  .catch(console.log)
+}
+
+const registerAuthentication = (req, res, db, bcrypt) => {
+  return handleRegister(req, res, db, bcrypt)
+    .then(data => {
+      return data.id && data.email ? createSessions(data) : Promise.reject(data)
+    })
+    .then(session => res.json(session))
+    .catch(err => res.status(400).json(err))
 }
 
 module.exports = {
-    handleRegister: handleRegister
+  registerAuthentication
 };
+
+// const handleRegister = (req, res, db, bcrypt) => {
+//   const { email, name, password } = req.body;
+//   if (!email || !name || !password) {
+//       return res.status(400).json('incorrect form submission');
+//   }
+//   const hash = bcrypt.hashSync(password);
+//       return db.transaction(trx => {
+//           trx.insert({ hash, email })
+//           .into('login')
+//           .returning('email')
+//           .then(loginEmail => {
+//               return trx('users')
+//               .returning('*')
+//               .insert({
+//                   email: loginEmail[0],
+//                   name: name,
+//                   joined: new Date()   
+//               })
+//               .then(user => user[0]);
+//           })
+//           .then(trx.commit)
+//           .catch(trx.rollback)
+//       })
+
+//   .catch(err => res.status(400).json('unable to register'))
+// }
+
+// module.exports = {
+//     handleRegister: handleRegister
+// };
--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(morgan('combined'));
 
 app.get('/', (req,res) => res.send('it is working!'))
 app.post('/signin', signin.signinAuthentication(db, bcrypt))
-app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
+app.post('/register', (req, res) => { register.registerAuthentication(req, res, db, bcrypt) })
 app.get('/profile/:id', auth.requireAuth, (req, res) => { profile.handleProfileGet(req, res, db) })
 app.post('/profile/:id', auth.requireAuth, (req, res) => { profile.handleProfileUpdate(req, res, db) })
 app.put('/image', auth.requireAuth, (req, res) => { image.handleImage(req, res, db) })
